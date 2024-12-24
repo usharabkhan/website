340710d1b44810e2379b8ae16a99016bc8670dec
usharabkhan <63792810+usharabkhan@users.noreply.github.com>
2024-12-24 16:03:07 -0700

    gsap in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,35 @@
 import MyHeading from "./common/heading";
 import { FaHandPointUp } from "react-icons/fa";
 import Socials from "./common/socials";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
 
 export default function Footer(): JSX.Element{
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#hand",
+                onEnter: () => tl.play(),
+                onLeave: () => tl.pause(),
+                onEnterBack: () => tl.play(),
+                onLeaveBack: () => tl.pause(),
+            }
+        });
+        tl.fromTo("#hand", 
+            { y: -10 }, 
+            { duration: 1, y: 0, repeat: -1, yoyo: true, ease: "power1-inOut" }
+        );
+        return () => {
+            tl.kill();
+        }
+    }, []);
     return(
         <div className="flex flex-col items-center justify-center p-5 text-white w-full h-fit" >
             <MyHeading type="h2" title="Hire Me!" />
             <Socials />
-            <FaHandPointUp size="50" className="text-yellow-400"/>
+            <FaHandPointUp id="hand" size="50" className="text-yellow-400"/>
         </div>
     )
 }
