0266eea0134ca9acc7be634d77ee7d49252451b5
usharabkhan <63792810+usharabkhan@users.noreply.github.com>
2025-11-18 12:20:56 -0700

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Theme>
         <App />
       </Theme>
     </BrowserRouter>
-  </StrictMode>,
-)
+    <Analytics />
+    <SpeedInsights />
+  </StrictMode>
+);
